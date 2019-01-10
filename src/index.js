995961307c0f4734af573f2d11d6a4e618192e35
Kc <kc.liang@jam3.com>
2019-01-10 16:31:06 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@babel/polyfill';
+import App from './App';
+import './style.css';
+import DefaultErrorBoundary from './DefaultErrorBoundary';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
